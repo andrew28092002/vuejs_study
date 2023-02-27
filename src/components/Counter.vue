5c5249c45f2b170c8ba183f2d46730ad074fedfd
andrew28092002 <andreiastanaev2002@gmail.com>
2023-02-27 15:40:07 +0500

    init

---
+++ src/components/Counter.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="center">
+        <h1>{{count}}</h1>
+        <button @click="increment" id="increment">increment</button>
+        <button @click="decrement" id="decrement">decrement</button>
+    </div>
+</template>
+
+<script>
+
+    export default {
+        name: 'counter',
+        data(){
+            return {
+                count: 0
+            }
+        },
+        methods: {
+            increment(){
+                this.count++
+            },
+            decrement(){
+                this.count--
+            }
+        }
+    }
+
+</script>
