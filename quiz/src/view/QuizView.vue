44dfefe82cf5bd91ede78a9db470812dc3557861
andrew28092002 <andreiastanaev2002@gmail.com>
2023-02-28 15:05:31 +0500

    update quiz-header

--- quiz/src/view/QuizView.vue
+++ quiz/src/view/QuizView.vue
@@ -1,9 +1,34 @@
 <template>
-    <div>
-        
-    </div>
+  <QuizHeader :questionStatus="questionStatus" :barPercentage="barPercentage" />
 </template>
 
 <script setup>
+import { ref } from "vue";
+import { useRouter } from "vue-router";
+import quizes from "./../data/quizes.json";
+import QuizHeader from "@/component/QuizHeader.vue";
 
+const router = useRouter();
+const quizId = parseInt(router.params.id);
+const quiz = quizes.find((quiz) => quiz.id === quizId);
+const currentQuestionIndex = ref(0);
+const result = ref(0);
+const showResults = ref(false);
+
+const questionStatus = computed(
+  () => `${currentQuestionIndex.value}/${quiz.questions.length}`
+);
+const barPercentage = computed(
+  () => `${(currentQuestionIndex.value / quiz.questions.length) * 100}%`
+);
+
+const onOptionSelected = (isCorrect) => {
+  if (isCorrect) {
+    numberOfCorrectAnswers.value++;
+  }
+  if (quiz.questions.length - 1 === currentQuestionIndex.value) {
+    showResults.value = true;
+  }
+  currentQuestionIndex.value++;
+};
 </script>
