4eca47781d54dd87141ee2b4f7e2bedbb0dcaf1a
andrew28092002 <andreiastanaev2002@gmail.com>
2023-02-28 09:42:33 +0500

    create header component

--- quiz/src/App.vue
+++ quiz/src/App.vue
@@ -1,26 +1,26 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<template lang="">
+  <div class="container">
+    <header>
+      <h1>Quizes</h1>
+      <input type="text" />
+    </header>
+    <div class="options-container"></div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue';
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    Header
   }
-}
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.container {
+  max-width: 1000px;
+  margin: 0 auto;
 }
 </style>
