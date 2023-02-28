8fc7b9ecc21f46cc45d65dba26af2940f184fbe6
andrew28092002 <andreiastanaev2002@gmail.com>
2023-02-28 16:18:30 +0500

    create result-component

---
+++ quiz/src/component/QuizResult.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="results">
+    <p>Your Results...</p>
+    <h1>{{ numberOfCorrectAnswers }}/{{ quizQuestionLength }}</h1>
+    <RouterLink to="/">Go Back</RouterLink>
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from "vue";
+
+const { quizQuestionLength, numberOfCorrectAnswers } = defineProps([
+  "numberOfCorrectAnswers",
+  "quizQuestionLength",
+]);
+</script>
+
+<style scoped>
+.results {
+  text-align: center;
+  padding: 100px 0;
+}
+p {
+  font-size: 25px;
+}
+h1 {
+  font-size: 80px;
+}
+</style>
